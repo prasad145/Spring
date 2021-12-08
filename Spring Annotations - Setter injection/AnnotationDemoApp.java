98922f837551da5f083041df0cc7715838166a74
Prasad Acharya <50115378+prasad145@users.noreply.github.com>
2021-12-09 00:45:22 +0530

    Annotations - Setter injection
    using @Autowired and @Component annotations to achieve dependency injection

---
+++ Spring Annotations - Setter injection/AnnotationDemoApp.java
@@ -0,0 +1,18 @@
+package com.springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+	public static void main(String[] args)
+	{
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach coach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(coach.getDailyWorkout());
+		
+		System.out.println(coach.getDailyFortune());
+		
+		context.close();
+	}
+}
