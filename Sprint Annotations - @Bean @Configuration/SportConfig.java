d3587d0df46f1384414bb11b33fcf12b77ad40b7
Prasad Acharya <50115378+prasad145@users.noreply.github.com>
2022-08-14 14:08:47 +0530

    Create SportConfig.java

---
+++ Sprint Annotations - @Bean @Configuration/SportConfig.java
@@ -0,0 +1,21 @@
+package com.springDemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+	@Bean
+	public FortuneService sadFortuneService()
+	{
+		return new SadFortuneService();
+	}
+	
+	@Bean
+	public Coach swimCoach()
+	{
+		return new SwimCoach(sadFortuneService());
+	}
+}
