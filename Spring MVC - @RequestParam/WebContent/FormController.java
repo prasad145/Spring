9e0afeb83f73577b3264f4b19f85e7f3f8a4ced3
Prasad Acharya <50115378+prasad145@users.noreply.github.com>
2022-08-21 19:35:12 +0530

    Add files via upload

---
+++ Spring MVC - @RequestParam/WebContent/FormController.java
@@ -0,0 +1,38 @@
+package com.spring.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FormController {
+	
+	@RequestMapping("/showForm")
+	public String showForm()
+	{
+		return "simple-form";
+	}
+	
+	// parsing request params using request object of http
+	@RequestMapping("/processForm")
+	public String displayForm(HttpServletRequest req, Model model)
+	{
+		String name = req.getParameter("yourName");
+		
+		model.addAttribute("message", "Yo! " + name.toUpperCase());
+		
+		return "display-form";
+	}
+	
+	// parsing request params using @RequestParam Annotation
+	@RequestMapping("/anotherProcessForm")
+	public String displayAnotherForm(@RequestParam("yourName") String name, Model model)
+	{
+		model.addAttribute("message", "Yo! " + name.toUpperCase());
+		
+		return "display-form";
+	}
+}
---
+++ Spring MVC - @RequestParam/WebContent/HomeController.java
@@ -0,0 +1,14 @@
+package com.spring.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String displayHomePage()
+	{
+		return "home-page";
+	}
+}
